80b5a47aec38726b6c9061376b5b4e5872340698
Ttibsi <ashisbitt@icloud.com>
2023-05-19 11:58:43 +0100

    Stack and Queue now complete

--- Technical_Documentation/last_algos_course_FEM/5_queue.py
+++ Technical_Documentation/last_algos_course_FEM/5_queue.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
+
 @dataclass
 class Node:
     value:Optional[int]
@@ -17,7 +18,7 @@ class Queue:
         return self.head.value if self.head else None
 
     def deque(self) -> Optional[int]:
-         not self.head:
+        if not self.head:
           return None 
 
         self.length -= 1
@@ -31,7 +32,7 @@ class Queue:
 
     def enqueue(self, item:int) -> None:
         self.length += 1
-        n = Node(value=item)
+        n = Node(item, None)
 
         if self.tail:
             self.tail.next = n
@@ -39,3 +40,34 @@ class Queue:
         else:
             self.tail = self.head = n
 
+    def items(self) -> list[int]:
+        ret = []
+
+        ptr = self.head
+        if ptr.value is None:
+            return ret
+
+        while True:
+            ret.append(ptr.value)
+            if ptr == self.tail:
+                break
+
+            ptr = ptr.next
+
+        return ret
+
+
+def main() -> None:
+    q = Queue()
+
+    for i in range(0, 5):
+        q.enqueue(i)
+
+    print(q.items())
+    print(q.peek())
+
+    q.deque()
+    print(q.items())
+
+if __name__ == "__main__":
+    main()
--- Technical_Documentation/last_algos_course_FEM/6_stack.py
+++ Technical_Documentation/last_algos_course_FEM/6_stack.py
@@ -1,31 +1,28 @@
 from dataclasses import dataclass
 from typing import Optional
 
-
 @dataclass
 class Node:
     value:Optional[int]
     prev: Optional["Node"]
 
+
 class Stack():
-    def __init__(self, head: Node):
+    def __init__(self):
         self.length:int = 0
         self.head:Optional[Node] = None
-
 
     def peek(self) -> Optional[int]:
         return self.head.value if self.head else None
 
-
     def push(self, item: int) -> None:
-        n = Node(value=item)
+        n = Node(item, None)
         self.length += 1
 
         if self.head:
             n.prev = self.head
 
         self.head = n
-
 
     def pop(self) -> Optional[int]:
         # Can't have negative length
@@ -39,3 +36,34 @@ class Stack():
         self.head = self.head.prev
         return self.head.value
 
+    def items(self) -> list[int]:
+        ret = []
+        ptr = self.head
+        if ptr.value is None:
+            return ret
+
+        while True:
+            ret.append(ptr.value)
+            if not ptr.prev: 
+                break
+
+            ptr = ptr.prev
+
+        return ret
+
+
+def main() -> None:
+    s = Stack()
+
+    for i in range(0, 5):
+        s.push(i)
+
+    print(s.items())
+    print(s.peek())
+
+    s.pop()
+    print(s.items())
+
+
+if __name__ == "__main__":
+    main()
