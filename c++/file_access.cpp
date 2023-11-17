d2c42355e40373c7ecf058c33cda9f22e5aad5b1
ttibsi <ashisbitt@icloud.com>
2023-11-17 14:26:12 +0000

    file access cpp check script

---
+++ c++/file_access.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+// Quick way to check if the current user has write access to a given file
+
+int main(int argc, char** argv) {
+    for (int i = 1; i < argc; i++) {
+        // man access
+        access(argv[i], W_OK); // switch to R_OK for read access
+        std::cout << argv[i] << ": " << strerror(errno) << "\n";
+    }
+
+    return 0;
+}
