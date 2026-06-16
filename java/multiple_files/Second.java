f17d869f5c42d73ee80509767983180ebf0e04a0
Ttibsi <ashisbitt@icloud.com>
2026-06-16 09:54:59 +0000

    java multiple file example

---
+++ java/multiple_files/Second.java
@@ -0,0 +1,8 @@
+package multiple_files;
+
+public class Second {
+    public void f() {
+        System.out.println("Hi");
+    }
+}
+
---
+++ java/multiple_files/first.java
@@ -0,0 +1,11 @@
+package multiple_files;
+
+import multiple_files.Second;
+
+public class first {
+    public static void main(String... args) {
+        Second s = new Second();
+        s.f();
+    }
+}
+
